382cef579a9ed9380f333f4b078801c6dd7fcf59
Nguyen Ngoc Trong <nguyenngoctrong@Nguyen-Ngoc-Trong.local>
2019-11-29 15:27:22 +0700

    Example twig on symfony.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,10 +3,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @return Response
@@ -18,14 +19,19 @@ class ArticleController
 
     /**
      * @Route("/news/{slug}")
-     * @return Response
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show one space article: %s',
-            $slug
-        ));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments
+        ]);
     }
 
 }
